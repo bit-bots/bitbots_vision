94be9e948db08d75703924a440765f7da0cc1d9e
Florian <florian@flova.de>
2019-05-05 00:16:31 +0200

    Merge and test

--- bitbots_vision/src/bitbots_vision/vision_modules/body_mask_ball_candidate_filter.py
+++ bitbots_vision/src/bitbots_vision/vision_modules/body_mask_ball_candidate_filter.py
@@ -1,15 +1,20 @@
-#! /usr/bin/env python2
+#!/usr/bin/env python2
 # TODO: remove
+
+import rospy
+import math
+import numpy as np
+import cv2
+from bitbots_vision.vision_modules import debug, ball, candidate
+
+import tf2_ros as tf2
+from tf2_geometry_msgs import PointStamped
+
 
 # TODO: docs
 # TODO: set resolution on transformer stuff
 # TODO: publish debug image as overlay of body mask with 0.5 opacity
 
-import cv2
-import math
-import rospy
-import numpy as np
-from bitbots_vision.vision_modules import horizon, debug, ball, candidate
 
 class body_mask_ball_candidate_filter(object):
     """
@@ -24,13 +29,17 @@ class body_mask_ball_candidate_filter(object):
         self.image_size = image_size
         # TODO dyn reconf
         self.max_intersection_threshold = 0.2
+        self.finder = BodyMaskObjectFinder()
+        while not rospy.is_shutdown():
+            self.test()
+        
 
     def get_body_parts(self):
         # type: () -> [(int, int), (int, int), int]
         """
         TODO
         """
-        pass
+        return self.finder.work()
 
     def get_body_mask(self, body_parts):
         # type: ([(int, int), (int, int), int]) -> TODO
@@ -42,7 +51,12 @@ class body_mask_ball_candidate_filter(object):
         # Draw black lines for the parts of the own body
         for body_part in body_parts:
             cv2.line(mask, body_part[0], body_part[1], (0,0,0), thickness=body_part[2])
+        self.imshow(mask)
         return mask
+
+    def test(self):
+        objects = self.get_body_parts()
+        self.imshow(self.get_body_mask(objects))
     
     def get_ball_mask(self, ball_candidate):
         # type: () -> TODO
@@ -56,6 +70,10 @@ class body_mask_ball_candidate_filter(object):
         # Draw white circle representing a ball
         mask = cv2.circle(canvas, center, radius, (255, 255, 255), thickness=-1)
         return mask
+
+    def imshow(self, image):
+        cv2.imshow("image", image)
+        k = cv2.waitKey(1)
 
     def ball_candidate_not_on_own_body(self, ball_candidate, body_mask):
         # type: () -> bool
@@ -85,6 +103,85 @@ class body_mask_ball_candidate_filter(object):
             ball_candidate,
             body_mask)]
 
-# TODO: remove
-if __name__ == '__main__':
+
+
+class BodyMaskObjectFinder(object):
+    def __init__(self):
+        self.rate = 30
+        self.thickness_scalar = 200
+        self.vertical_fov = 70
+        self.horizontal_fov = int(90 * 1.7)
+        self.x_resolution = 640
+        self.y_resolution = 360
+        self.frame = 'camera_optical_frame'
+        self.tfBuffer = tf2.Buffer()
+        self.listener = tf2.TransformListener(self.tfBuffer)
+        rospy.sleep(0.5)
+
+    def set_resolution(self, x, y):
+        self.x_resolution = x
+        self.y_resolution = y
+
+    def start(self):
+        self.main_loop()
+
+    def main_loop(self):
+        rospy.sleep(0.5)
+        while not rospy.is_shutdown():
+            print(self.work())
+            # TODO nicer
+            rospy.sleep(1/float(self.rate))
+
+    def work(self):
+        joints = ['l_wrist', 'r_wrist', 'l_lower_arm', 'r_lower_arm']
+
+        joint_positions = map(self.transform_joint, joints)
+        
+        angles = map(self.calculate_arm_point_angle, joint_positions)
+
+        relative_image_points = map(self.image_position_from_angle, angles)
+        
+        absolute_image_points = map(self.to_absolute, relative_image_points)
+
+        distance = map(self.distance, joint_positions)
+
+        thickness = map(self.distance_to_thickness, distance)
+
+        left_arm = (absolute_image_points[0], absolute_image_points[2], thickness[0])
+        right_arm = (absolute_image_points[1], absolute_image_points[3], thickness[1])
+
+
+        objects = [left_arm, right_arm]
+
+        print(objects)
+        return objects
+
+    def distance_to_thickness(self, distance):
+        return int(1/((distance+1)**2) * self.thickness_scalar)
+
+    def to_absolute(self, point):
+        return int(point[0] + (self.x_resolution/2)), int(point[1] + (self.y_resolution/2))
+
+    def distance(self, relative_joint_position):
+        rjp = relative_joint_position
+        return math.sqrt(rjp.x**2 + rjp.y**2 + rjp.z**2)
+
+    def transform_joint(self, joint):
+        #TODO try
+        return self.tfBuffer.lookup_transform(self.frame, joint, rospy.Time(0)).transform.translation
+
+    def calculate_arm_point_angle(self, relative_joint_position):
+        y_rotation = math.atan2(float(relative_joint_position.x), float(relative_joint_position.z))
+        x_rotation = math.atan2(float(relative_joint_position.y), float(relative_joint_position.z))
+        # Finde why its off by factor 2
+        return x_rotation, y_rotation
+
+    def image_position_from_angle(self, angle):
+        scalar_x = float(self.x_resolution)/self.horizontal_fov
+        scalar_y = float(self.y_resolution)/self.vertical_fov
+        return int(math.degrees(angle[1]) * scalar_x), int(math.degrees(angle[0]) * scalar_y)
+
+
+if __name__ == "__main__":
+    rospy.init_node('body_mask')
     body_mask_ball_candidate_filter((360, 640))
