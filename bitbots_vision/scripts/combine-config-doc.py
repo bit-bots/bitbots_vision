2d7bd362996b0dbc21ee7689b5d5529b3766ee0a
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2019-09-14 23:27:42 +0200

    adds warning for empty description

--- bitbots_vision/scripts/combine-config-doc.py
+++ bitbots_vision/scripts/combine-config-doc.py
@@ -1,24 +1,30 @@
 #! /usr/bin/env python3
 import os
 import re
-import yaml
+import sys
 import argparse
 
 """
 A script to combine the parameter description (documentation) of .yaml and dynamic reconfigurable .cfg files.
-        This script compares the parameter descriptions of the SOURCE file with the related ones of the DESTINATION file.
+        This script copies the parameter descriptions of the SOURCE file to the related ones of the DESTINATION file.
         If SAVE, the DESTINATION file will be overwritten.
 """
 
 
 def main(source_path, destination_path):
     # Parse files for keys and associated description
-    print(f"Analysing '{source_path}'...")
+    print(f"\nAnalysing '{source_path}'...")
     if source_path.endswith(".yaml"):
         source = parse_yaml(source_path)
     elif source_path.endswith(".cfg"):
         source = parse_cfg(source_path)
     print(f"Found {len(source)} parameters in '{source_path}'.")
+
+    # Warn, if parameter has no description
+    no_description = [key for key in source if source[key] == ""]
+    if no_description:
+        print_warn(f"The following {len(no_description)} parameters have no description in SOURCE:")
+        print(no_description)
 
     print(f"\nAnalysing '{destination_path}'...")
     if destination_path.endswith(".yaml"):
@@ -27,17 +33,12 @@ def main(source_path, destination_path):
         destination = parse_cfg(destination_path)
     print(f"Found {len(destination)} parameters in '{destination_path}'.")
 
+    no_description = [key for key in destination if destination[key] == ""]
+    if no_description:
+        print(f"{len(no_description)} parameters have no description in DESTINATION.")
+
     result = compare(source, destination)
     save(destination_path, result)
-
-
-def compare(source, destination):
-    print(f"\nComparing SOURCE and DESTINATION...")
-
-    # TODO: compare
-
-    print("Finished comparing.")
-    # TODO: print statistics
 
 
 def parse_yaml(file_path):
@@ -83,6 +84,16 @@ def parse_cfg(file_path):
     return key_description
 
 
+def compare(source, destination):
+    print(f"\nComparing descriptions of SOURCE and DESTINATION...")
+
+    for key, description in source.items():
+        pass
+
+    print("Finished comparing.")
+    # TODO: print statistics
+
+
 def save(destination, result):
     print(f"\nSaving to '{destination}'...")
 
@@ -100,9 +111,13 @@ def save(destination, result):
     print("Finished saving.")
 
 
+def print_warn(message):
+    sys.stderr.write('\x1b[1;33m' + message + '\x1b[0m' + "\n")
+
+
 if __name__ == "__main__":
     main_description = "Combine the parameter description (documentation) of .yaml and dynamic reconfigurable .cfg files. " +\
-        "This script compares the parameter descriptions of the SOURCE file with the related ones of the DESTINATION file. " +\
+        "This script copies the parameter descriptions of the SOURCE file with the related ones of the DESTINATION file. " +\
         "If SAVE, the DESTINATION file will be overwritten."
 
     parser = argparse.ArgumentParser(
