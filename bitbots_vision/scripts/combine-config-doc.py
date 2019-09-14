ba67b42665cfed7120b41a265809104bb628ecf5
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2019-09-14 22:07:15 +0200

    adds parsing of cfg

--- bitbots_vision/scripts/combine-config-doc.py
+++ bitbots_vision/scripts/combine-config-doc.py
@@ -2,7 +2,6 @@
 import os
 import re
 import yaml
-import pickle
 import argparse
 
 """
@@ -11,36 +10,76 @@ A script to combine the parameter description (documentation) of .yaml and dynam
         If SAVE, the DESTINATION file will be overwritten.
 """
 
-def parse_yaml(file):
-    pass
 
-def parse_cfg(file):
-    pass
-
-def compare(source_path, destination_path):
-    print(f"Comparing '{source_path}' with '{destination_path}'...")
-
+def main(source_path, destination_path):
+    # Parse files for keys and associated description
     if source_path.endswith(".yaml"):
         source = parse_yaml(source_path)
     elif source_path.endswith(".cfg"):
         source = parse_cfg(source_path)
 
     if destination_path.endswith(".yaml"):
-        destiniation = parse_yaml(destination_path)
+        destination = parse_yaml(destination_path)
     elif destination_path.endswith(".cfg"):
         destination = parse_cfg(destination_path)
+
+    result = compare(source, destination)
+    save(destination_path, result)
+
+
+def compare(source, destination):
+    print(f"\nComparing SOURCE and DESTINATION...")
 
     # TODO: compare
 
     print("Finished comparing.")
     # TODO: print statistics
 
+
+def parse_yaml(file):
+    pass
+
+
+def parse_cfg(file_path):
+    print(f"\nAnalysing '{file_path}'...")
+    # Search for parameters in .cfg file
+    parameters = []
+    # Load line by line
+    lines = tuple(open(file_path, 'r'))
+    for line in lines:
+        if re.search(r"\.add\(", line):
+            parameters.append(re.split(r".*\.add\(", line)[1])
+
+    # Extract key and description from each parameter
+    key_description = {}
+    for parameter in parameters:
+        arguments = re.split(r",\s", parameter)
+        # Key at argument position 0, remove quotation around in "key"
+        key = arguments[0][1:-1]
+        # Description at argument position 3, remove quotation around in "description"
+        description = arguments[3][1:-1]
+        key_description[key] = description
+    print(f"Found {len(key_description)} parameters in '{file_path}'.")
+
+    return key_description
+
+
 def save(destination, result):
-    print(f"Saving to '{destination}'...")
+    print(f"\nSaving to '{destination}'...")
+
+    """# Ask user if he wants to save it
+    if args.no_confirm or input("\n {} parameters changed. Do you want to save? (y/n)".format(changed_params)) == "y":
+        # Save new file
+        with open(config_path, "w") as fp:
+            yaml.dump(data, fp)
+        print("Saved file")
+    else:
+        print("Nothing has been saved!")"""
 
     # TODO: save
 
     print("Finished saving.")
+
 
 if __name__ == "__main__":
     main_description = "Combine the parameter description (documentation) of .yaml and dynamic reconfigurable .cfg files. " +\
@@ -49,15 +88,11 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
         description=main_description)
-    parser.add_argument("-s", "--save", action="store_true",
-                        help="Save the combined output in the destination_path file (Default: False)")
     parser.add_argument("-S", "--source", action="store", nargs=1, type=str,
                         help="Source file of the parameter description")
     parser.add_argument("-D", "--destination", action="store", nargs=1, type=str,
                         help="Destination file of the parameter description")
     args = parser.parse_args()
-
-    print(args) # TODO: remove
 
     # Error handling
     if args.source is None or args.destination is None:
@@ -73,34 +108,5 @@ if __name__ == "__main__":
             (destination_path.endswith(".yaml") or destination_path.endswith(".cfg"))):
         raise argparse.ArgumentError(None, f"SOURCE and DESTINATION file must be '.yaml' OR '.cfg' files.")
 
-    result = compare(source_path, destination_path)
+    main(source_path, destination_path)
 
-    if args.save:
-        save(destination_path, result)
-
-"""
-# Compatibility
-try:
-    input = raw_input
-except:
-    pass
-
-# Argument parsing
-parser = argparse.ArgumentParser()
-parser.add_argument("-i", "--ignore-debug", help="Ignore debug params", action='store_true')
-parser.add_argument("--no-confirm", help="Do not ask for write permission", action='store_true')
-args = parser.parse_args()
-
-# Open old config file
-with open(config_path) as fp:
-    data = yaml.load(fp)
-
-# Ask user if he wants to save it
-if args.no_confirm or input("\n {} parameters changed. Do you want to save? (y/n)".format(changed_params)) == "y":
-    # Save new file
-    with open(config_path,"w") as fp:
-        yaml.dump(data, fp)
-    print("Saved file")
-else:
-    print("Nothing has been saved!")
-"""
