0f7506d6f08a8572e85ce80f3e631b5be0d710ca
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-01-08 17:54:00 +0100

    Fix: adds images folder

--- bitbots_vision_tools/scripts/imgclean.sh
+++ bitbots_vision_tools/scripts/imgclean.sh
@@ -11,11 +11,14 @@ Trash="unusable"
 
 debug="./debug_$Trash"
 labels="./labels_$Trash"
+images="./images_$Trash"
 
 mkdir -p $debug
 mkdir -p $labels
+mkdir -p $images
 
 mv_debug='mv ./debug/$(basename "%f")'
 mv_labels='mv ./labels/$(basename "%f")'
+mv_images='mv ./images/$(basename "%f")'
 
-feh -Z -F -d --action1 "$mv_debug $debug && $mv_labels $labels" ./debug/*
+feh -Z -F -d --action1 "$mv_debug $debug && $mv_labels $labels && $mv_images $images" ./debug/*
