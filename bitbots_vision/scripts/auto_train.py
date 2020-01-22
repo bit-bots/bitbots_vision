e048084be847234d46b99544d0ae46a9ef4f9b7a
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-01-22 16:34:53 +0100

    autotrain: fix model config

--- bitbots_vision/scripts/auto_train.py
+++ bitbots_vision/scripts/auto_train.py
@@ -17,7 +17,7 @@ default_config['input_width'] = 192  # Input width
 default_config['input_height'] = 192  # Input height
 default_config['train_images'] = "/srv/ssd_nvm/deep_field/data/group_all/images/"  # Path to image dataset
 default_config['train_annotations'] = "/srv/ssd_nvm/deep_field/data/group_all/labels/"  # Path to label dataset
-default_config['checkpoints_base_path'] = "/srv/ssd_nvm/deep_field/models/16_01_20/"  # Path to store checkpoints
+default_config['checkpoints_base_path'] = "/srv/ssd_nvm/deep_field/models/foo/"  # Path to store checkpoints
 
 
 # Model config
@@ -27,8 +27,8 @@ models['fcn_8'] = {}
 models['fcn_32'] = {}
 models['fcn_8_vgg'] = {}
 models['fcn_32_vgg'] = {}
-models['fcn_8_mobilenet'] = {}
-models['fcn_32_mobilenet'] = {}
+models['fcn_8_mobilenet'] = {'input_width': 224, 'input_height': 224}
+models['fcn_32_mobilenet'] = {'input_width': 224, 'input_height': 224}
 models['pspnet'] = {'input_width': 192, 'input_height': 192}
 models['vgg_pspnet'] = {'input_width': 192, 'input_height': 192}
 models['unet_mini'] = {}
@@ -40,8 +40,6 @@ models['vgg_segnet'] = {}
 
 # Models, that run only on the CPU
 cpu_models = {}
-cpu_models['fcn_8_resnet50'] = {}
-cpu_models['fcn_32_resnet50'] = {}
 cpu_models['vgg_unet'] = {}
 
 # Models, that do not run for some reason
@@ -51,6 +49,8 @@ blacklist_models['pspnet_50'] = {}  # Pooling parameters for input shape  (192, 
 blacklist_models['pspnet_101'] = {}  # Pooling parameters for input shape  (192, 192)  are not defined.
 blacklist_models['resnet50_unet'] = {}  # ValueError: Negative dimension size caused by subtracting 7 from 6 for 'avg_pool/AvgPool' (op: 'AvgPool') with input shapes: [?,6,6,2048].
 blacklist_models['resnet50_segnet'] = {}  # ValueError: Negative dimension size caused by subtracting 7 from 6 for 'avg_pool/AvgPool' (op: 'AvgPool') with input shapes: [?,6,6,2048].
+blacklist_models['fcn_8_resnet50'] = {}  # CPU and ValueError: Negative dimension size caused by subtracting 7 from 6 for 'avg_pool/AvgPool' (op: 'AvgPool') with input shapes: [?,6,6,2048].
+blacklist_models['fcn_32_resnet50'] = {}  # CPU and ValueError: Negative dimension size caused by subtracting 7 from 6 for 'avg_pool/AvgPool' (op: 'AvgPool') with input shapes: [?,6,6,2048].
 
 
 # Train models
@@ -78,8 +78,10 @@ def train(models):
 
         del model
 
+# Train on GPU 0
+os.environ['CUDA_VISIBLE_DEVICES'] = "0"
 train(models)
 
 # Train on CPU
-os.environ['CUDA_VISIBLE_DEVICES'] = -1
+os.environ['CUDA_VISIBLE_DEVICES'] = "-1"
 train(cpu_models)
