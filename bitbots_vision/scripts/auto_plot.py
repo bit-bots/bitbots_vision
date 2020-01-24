079643b5cdefa9928d4612a202bc44063d491e3e
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-01-24 23:14:43 +0100

    update plot

--- bitbots_vision/scripts/auto_plot.py
+++ bitbots_vision/scripts/auto_plot.py
@@ -18,20 +18,39 @@ plotly_filename = "accuracy_plot.html"
 
 models = [
         'fcn_8',
-        'fcn_32',
         'fcn_8_vgg',
-        'fcn_32_vgg',
         'fcn_8_mobilenet',
+        'fcn_32',
+        'fcn_32_vgg',
         'fcn_32_mobilenet',
-        'pspnet',
-        'vgg_pspnet',
+        'mobilenet_unet',
+        'vgg_unet',
         'unet_mini',
         'unet',
-        'mobilenet_unet',
-        'mobilenet_segnet',
         'segnet',
+        'mobilenet_segnet',
         'vgg_segnet',
-        'vgg_unet']
+        'vgg_pspnet',
+        'pspnet'
+        ]
+
+choosen_models = [
+        # 'fcn_8',
+        # 'fcn_8_vgg',
+        # 'fcn_8_mobilenet',
+        # 'fcn_32',
+        # 'fcn_32_vgg',
+        # 'fcn_32_mobilenet',
+        # 'mobilenet_unet',
+        # 'vgg_unet',
+        'unet_mini',
+        # 'unet',
+        # 'segnet',
+        'mobilenet_segnet',
+        'vgg_segnet',
+        # 'vgg_pspnet',
+        # 'pspnet'
+        ]
 
 
 # Load evaluation data
@@ -51,12 +70,27 @@ for model, data in models_data.items():
         x=list(range(max_epochs)),
         y=[datapoint[select_data] for datapoint in data],
         mode='lines',
-        name=model))
+        name=model.upper()))
 
 fig.add_trace(go.Scatter(
         x=list(range(max_epochs)),
         y=[const_vision_value]*max_epochs,
         mode='lines',
-        name='bitbots_vision'))
+        name='bitbots_vision',
+        line = dict(color='red', width=5)))
+
+fig.update_layout(
+        font=dict(
+                family="Courier New, monospace",
+                size=25,
+                color="#7f7f7f"),
+        paper_bgcolor='rgba(0,0,0,0)',
+        plot_bgcolor='rgba(0,0,0,0)')
+
+fig.update_xaxes(title_text="Epoch", gridwidth=1)
+fig.update_yaxes(title_text="Accuracy", gridwidth=1)
+
+# fig.write_image("~/accuracy.pdf")
+
 
 py.plot(fig, filename=plotly_filename, auto_open=True)
