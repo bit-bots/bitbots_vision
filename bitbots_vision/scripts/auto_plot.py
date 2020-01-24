a96c1821043740f7c58daadcaba82b7bb3f11318
Jan Gutsche <7gutsche@informatik.uni-hamburg.de>
2020-01-24 21:47:20 +0100

    adds all models to autoplot

--- bitbots_vision/scripts/auto_plot.py
+++ bitbots_vision/scripts/auto_plot.py
@@ -13,37 +13,50 @@ import plotly.graph_objs as go
 const_vision_value = 0.925
 evaluation_file = "/home/jan/eval.yaml"
 max_epochs = 25
-select_model = "fcn_32"
 select_data = "mean_IU"
-plotly_filename = "plotly_{}.html".format(select_model)
+plotly_filename = "accuracy_plot.html"
+
+models = [
+        'fcn_8',
+        'fcn_32',
+        'fcn_8_vgg',
+        'fcn_32_vgg',
+        'fcn_8_mobilenet',
+        'fcn_32_mobilenet',
+        'pspnet',
+        'vgg_pspnet',
+        'unet_mini',
+        'unet',
+        'mobilenet_unet',
+        'mobilenet_segnet',
+        'segnet',
+        'vgg_segnet',
+        'vgg_unet']
 
 
 # Load evaluation data
 with open(evaluation_file, 'r') as file:
     evaluation_data = yaml.full_load(file)
 
-# Select model
-model_data = []
-for i in range(max_epochs):
-    model_data.append(evaluation_data["{}.{}".format(select_model, i)])
-
-# Select data group
-data_group = []
-for data in model_data:
-    data_group.append(data[select_data])
+# Extract model-specific data
+models_data = {}
+for model in models:
+    models_data[model] = [evaluation_data["{}.{}".format(model, i)] for i in range(max_epochs)]
 
 # Plot evaluation data
 # Create traces
 fig = go.Figure()
-fig.add_trace(go.Scatter(
-        x=list(range(len(data_group))),
-        y=data_group,
+for model, data in models_data.items():
+    fig.add_trace(go.Scatter(
+        x=list(range(max_epochs)),
+        y=[datapoint[select_data] for datapoint in data],
         mode='lines',
-        name=select_model))
+        name=model))
+
 fig.add_trace(go.Scatter(
-        x=list(range(len(data_group))),
-        y=[const_vision_value]*len(data_group),
+        x=list(range(max_epochs)),
+        y=[const_vision_value]*max_epochs,
         mode='lines',
-        name='const_vision'))
+        name='bitbots_vision'))
 
 py.plot(fig, filename=plotly_filename, auto_open=True)
